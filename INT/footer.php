5b2fff0e7014129e5547934c92b46652ac38f30e
61160287 <70052050+61160287@users.noreply.github.com>
2022-04-05 14:57:16 +0700

    Update footer.php

--- INT/footer.php
+++ INT/footer.php
@@ -1,3 +1,14 @@
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+}
+</style>
+</head>
 <div class="footer">
     <div class="pull-right">
         <a href="http://hris" target="bank_">HUMAN RESOURCES INFORMATION SYSTEM </a>
